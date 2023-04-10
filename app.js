213f829448674a050bab11bf4f808b13aec0d939
Hope <hopeolaide@gmail.com>
2023-04-09 20:14:07 -0400

    Add event listener for circle/nought moves.

--- app.js
+++ app.js
@@ -17,9 +17,16 @@ function createGrid() {
     // Use position(index) to assign an id to each tile element (each square)
     tileElement.id = position;
     tileElement.textContent = position + 1; // Display the visible tile number (Different from index position!)
+    tileElement.addEventListener("click", nextMove);
     gameArena.append(tileElement);
   });
 }
 
 createGrid();
 
+function nextMove(e) {
+  // console.log(e.target) /* Use to check/test which tile has been selected
+  const moveDisplay = document.createElement("div");
+  moveDisplay.classList.add("circle");
+  e.target.append(moveDisplay);
+}
