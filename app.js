90248ca6cc6f1ec570a53f0fb7e06b44e816a66d
Hope <hopeolaide@gmail.com>
2023-04-16 08:09:50 -0400

    "Refactor createGrid() and update comments."

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function createGrid() {
     tileElement.classList.add("tile");
     // Use position(index) to assign an id to each tile element (each square)
     tileElement.id = position;
-    tileElement.textContent = position + 1; // Display the visible tile number (Different from index position!)
+    const tileNumber = tileElement.textContent = position + 1; // Display the visible tile number (Different from index position!)
     tileElement.addEventListener("click", addMove);
     gameArena.append(tileElement);
   });
