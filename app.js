29fe0c91640abcf1dfa7b104b337137ad17228d3
Hope <hopeolaide@gmail.com>
2023-04-16 07:59:46 -0400

    Refactor app.js: change nextMove to addMove. Add hard-coded tiles to separate html file.

--- app.js
+++ app.js
@@ -17,16 +17,21 @@ function createGrid() {
     // Use position(index) to assign an id to each tile element (each square)
     tileElement.id = position;
     tileElement.textContent = position + 1; // Display the visible tile number (Different from index position!)
-    tileElement.addEventListener("click", nextMove);
+    tileElement.addEventListener("click", addMove);
     gameArena.append(tileElement);
   });
 }
 
 createGrid();
 
-function nextMove(e) {
-  // console.log(e.target) /* Use to check/test which tile has been selected
+function addMove(e) {
+  // console.log(e.target) /* Use to check/test which tile has been selected */
   const moveDisplay = document.createElement("div");
   moveDisplay.classList.add("circle");
   e.target.append(moveDisplay);
 }
+
+// resetButton.addEventListener("click", () => {
+//   gameArena.innerHTML = "";
+//   createGrid();
+// });
